e503d6c70d9f2d271fc725645de6fc920818c364
DK22Pac <gdamdk@gmail.com>
2017-05-17 15:32:37 +0300

    change encoding

--- VC_PathsDivider/VC_PathsDivider.cpp
+++ VC_PathsDivider/VC_PathsDivider.cpp
@@ -6,46 +6,46 @@
 #include <QTextStream>
 
 VC_PathsDivider::VC_PathsDivider(QWidget *parent) : QMainWindow(parent) {
-    // Размеры и название окна
+    // Р Р°Р·РјРµСЂС‹ Рё РЅР°Р·РІР°РЅРёРµ РѕРєРЅР°
     setMinimumSize(400, 75);
     setMaximumSize(400, 75);
     setWindowTitle("GTA Vice City Paths Divider");
 
-    // Элементы - строка, 2 кнопки и переключатель
+    // Р­Р»РµРјРµРЅС‚С‹ - СЃС‚СЂРѕРєР°, 2 РєРЅРѕРїРєРё Рё РїРµСЂРµРєР»СЋС‡Р°С‚РµР»СЊ
     QLineEdit *lePath = new QLineEdit(this);
     QPushButton *btnBrowse = new QPushButton("Browse...", this);
     QPushButton *btnDivide = new QPushButton("Divide", this);
     QDoubleSpinBox *sbDivider = new QDoubleSpinBox(this);
 
-    // Расположение
+    // Р Р°СЃРїРѕР»РѕР¶РµРЅРёРµ
     lePath->setGeometry(10, 10, 300, 20);
     btnBrowse->setGeometry(320, 7, 70, 25);
     btnDivide->setGeometry(200, 40, 190, 25);
     sbDivider->setGeometry(10, 42, 180, 20);
 
-    // При нажатии на "Browse..."
+    // РџСЂРё РЅР°Р¶Р°С‚РёРё РЅР° "Browse..."
     connect(btnBrowse, &QPushButton::clicked, [=]() {
         lePath->setText(QFileDialog::getOpenFileName(this, "Select paths.ipl file", "", "IPL files (*.ipl)"));
     });
 
-    // При нажатии на "Divide"
+    // РџСЂРё РЅР°Р¶Р°С‚РёРё РЅР° "Divide"
     connect(btnDivide, &QPushButton::clicked, [=]() {
-        btnDivide->setEnabled(false); // временно "выключаем" кнопку
-        if (!lePath->text().isEmpty() && sbDivider->value() != 0 && sbDivider->value() != 1) { // если строка не пустая, а делитель не равен '0' или '1'
+        btnDivide->setEnabled(false); // РІСЂРµРјРµРЅРЅРѕ "РІС‹РєР»СЋС‡Р°РµРј" РєРЅРѕРїРєСѓ
+        if (!lePath->text().isEmpty() && sbDivider->value() != 0 && sbDivider->value() != 1) { // РµСЃР»Рё СЃС‚СЂРѕРєР° РЅРµ РїСѓСЃС‚Р°СЏ, Р° РґРµР»РёС‚РµР»СЊ РЅРµ СЂР°РІРµРЅ '0' РёР»Рё '1'
             QFile inputFile(lePath->text());
-            if (inputFile.open(QIODevice::ReadOnly)) { // если файл открылся
-                QStringList outputLines; // список строк (пока что пустой), которые будут записаны в выходной файл
-                while (!inputFile.atEnd()) { // пока не достигнем окончания файла
-                    outputLines.append(inputFile.readLine()); // записываем строку в список
-                    if (outputLines.last().startsWith("path")) { // если открывается секция 'path'
-                        outputLines.append(inputFile.readLine()); // следующая за 'path' строка
-                        while (!outputLines.last().startsWith("end")) { // читаем секцию, пока не наткнемся на 'end'
-                            for (unsigned int i = 0; i < 12; ++i) { // следуюшие 12 строк
-                                QString line = inputFile.readLine(); // получаем строку
-                                QStringList lineValues = line.split(','); // разбиваем на части
-                                for (unsigned int comp = 3; comp <= 5; ++comp) // делим 4-ый, 5-ый, 6-ой компонент (x,y,z)
+            if (inputFile.open(QIODevice::ReadOnly)) { // РµСЃР»Рё С„Р°Р№Р» РѕС‚РєСЂС‹Р»СЃСЏ
+                QStringList outputLines; // СЃРїРёСЃРѕРє СЃС‚СЂРѕРє (РїРѕРєР° С‡С‚Рѕ РїСѓСЃС‚РѕР№), РєРѕС‚РѕСЂС‹Рµ Р±СѓРґСѓС‚ Р·Р°РїРёСЃР°РЅС‹ РІ РІС‹С…РѕРґРЅРѕР№ С„Р°Р№Р»
+                while (!inputFile.atEnd()) { // РїРѕРєР° РЅРµ РґРѕСЃС‚РёРіРЅРµРј РѕРєРѕРЅС‡Р°РЅРёСЏ С„Р°Р№Р»Р°
+                    outputLines.append(inputFile.readLine()); // Р·Р°РїРёСЃС‹РІР°РµРј СЃС‚СЂРѕРєСѓ РІ СЃРїРёСЃРѕРє
+                    if (outputLines.last().startsWith("path")) { // РµСЃР»Рё РѕС‚РєСЂС‹РІР°РµС‚СЃСЏ СЃРµРєС†РёСЏ 'path'
+                        outputLines.append(inputFile.readLine()); // СЃР»РµРґСѓСЋС‰Р°СЏ Р·Р° 'path' СЃС‚СЂРѕРєР°
+                        while (!outputLines.last().startsWith("end")) { // С‡РёС‚Р°РµРј СЃРµРєС†РёСЋ, РїРѕРєР° РЅРµ РЅР°С‚РєРЅРµРјСЃСЏ РЅР° 'end'
+                            for (unsigned int i = 0; i < 12; ++i) { // СЃР»РµРґСѓСЋС€РёРµ 12 СЃС‚СЂРѕРє
+                                QString line = inputFile.readLine(); // РїРѕР»СѓС‡Р°РµРј СЃС‚СЂРѕРєСѓ
+                                QStringList lineValues = line.split(','); // СЂР°Р·Р±РёРІР°РµРј РЅР° С‡Р°СЃС‚Рё
+                                for (unsigned int comp = 3; comp <= 5; ++comp) // РґРµР»РёРј 4-С‹Р№, 5-С‹Р№, 6-РѕР№ РєРѕРјРїРѕРЅРµРЅС‚ (x,y,z)
                                     lineValues[comp] = QString(' ') + QString::number(lineValues[comp].toDouble() / sbDivider->value());
-                                outputLines.append(lineValues.join(',')); // собираем обратно в одну строку и добавляем в список
+                                outputLines.append(lineValues.join(',')); // СЃРѕР±РёСЂР°РµРј РѕР±СЂР°С‚РЅРѕ РІ РѕРґРЅСѓ СЃС‚СЂРѕРєСѓ Рё РґРѕР±Р°РІР»СЏРµРј РІ СЃРїРёСЃРѕРє
                             }
                             outputLines.append(inputFile.readLine());
                         }
@@ -55,15 +55,15 @@ VC_PathsDivider::VC_PathsDivider(QWidget *parent) : QMainWindow(parent) {
                 QString outputPath = QFileDialog::getSaveFileName(this, "Save to file", lePath->text(), "IPL files(*.ipl)");
                 if(!outputPath.isEmpty()){
                     QFile outputFile(outputPath);
-                    if (outputFile.open(QIODevice::WriteOnly)) { // если файл открылся
+                    if (outputFile.open(QIODevice::WriteOnly)) { // РµСЃР»Рё С„Р°Р№Р» РѕС‚РєСЂС‹Р»СЃСЏ
                         QTextStream outStream(&outputFile);
-                        for(QString &str : outputLines) // записываем строки из списка в новый файл
+                        for(QString &str : outputLines) // Р·Р°РїРёСЃС‹РІР°РµРј СЃС‚СЂРѕРєРё РёР· СЃРїРёСЃРєР° РІ РЅРѕРІС‹Р№ С„Р°Р№Р»
                             outStream << str;
                     }
                     outputFile.close();
                 }
             }
         }
-        btnDivide->setEnabled(true); // "включаем" кнопку обратно
+        btnDivide->setEnabled(true); // "РІРєР»СЋС‡Р°РµРј" РєРЅРѕРїРєСѓ РѕР±СЂР°С‚РЅРѕ
     });
 }
